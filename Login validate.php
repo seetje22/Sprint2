603288e300a5fea00b0d379ce25bb4bde4a288f1
seetje22 <95631839+seetje22@users.noreply.github.com>
2023-02-24 02:31:40 -0800

    Add files via upload

---
+++ Login validate.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Retrieve the username and password from the form
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // TODO: Validate the username and password with your backend database
+
+  // Dummy validation for demonstration purposes
+  if ($username === 'myusername' && $password === 'mypassword') {
+    // Authentication succeeded
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    header('Location: dashboard.php');
+  } else {
+    // Authentication failed
+    $_SESSION['loggedin'] = false;
+    $error_message = 'Invalid username or password';
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+  <h1>Login</h1>
+
+  <?php if (isset($error_message)): ?>
+    <p><?php echo $error_message; ?></p>
+  <?php endif; ?>
+
+  <form method="POST" action="">
+    <label>Username:</label>
+    <input type="text" name="username" required><br>
+
+    <label>Password:</label>
+    <input type="password" name="password" required><br>
+
+    <button type="submit">Login</button>
+  </form>
+</body>
+</html>
+
+Regenerate response
